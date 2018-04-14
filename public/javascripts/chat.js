119511d4ace8d6754cd23cec09968db53f8508f6
samah2407 <fathimahaga@gmail.com>
2018-04-14 12:15:27 +0000

    initial commit

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,4 +1,18 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 app.controller('chatController', function ($scope) {
+    $scope.messages = [
+        {
+            'sender': 'USER',
+            'text': 'Hello'
+		},
+        {
+            'sender': 'BOT',
+            'text': 'Hi!!How are u?'
+		},
+        {
+            'sender': 'USER',
+            'text': 'I am fine .Thank u.'
+		}
+	];
 
 });
